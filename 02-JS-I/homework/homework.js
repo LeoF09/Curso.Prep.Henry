dc24794073f7cea579c5488948d7e0160cd488aa
LeoF09 <alfernandez1409@gmail.com>
2021-07-01 13:36:54 -0300

    act2 homework

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,7 +1,7 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = Leo;
+const nuevaString = "Leo";
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = 09;
@@ -28,6 +28,7 @@ const nuevoModulo = 21 % 5 === 1;
 function devolverString(str) {
   // "Return" la string provista: str
   // Tu código:
+  return str;
 }
 
 function suma(x, y) {
